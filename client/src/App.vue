317d9a2c57978088f91a532ddc14b53e9b425540
georgemblack <contact@georgeblack.me>
2019-11-29 11:07:14 -0600

    Fetch views on page load

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    ...mapActions(['getAllViews'])
+  },
+  mounted() {
+    this.getAllViews()
+  }
 }
 </script>
 
