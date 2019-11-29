36c73a34d862803e91f96ace50a6ecb06c21888d
georgemblack <contact@georgeblack.me>
2019-11-29 11:17:15 -0600

    Experiment with Bulma CSS

--- client/src/views/ViewTable.vue
+++ client/src/views/ViewTable.vue
@@ -3,23 +3,27 @@
     <button @click="getAllViews">
       Refresh
     </button>
-    <table>
-      <tr>
-        <th>Timestamp</th>
-        <th>Time Zone</th>
-        <th>Path</th>
-        <th>Referrer</th>
-        <th>Browser</th>
-        <th>Engine</th>
-        <th>OS</th>
-        <th>Vendor</th>
-        <th>Window</th>
-      </tr>
-      <view-table-row
-        v-for="view in allViews"
-        :key="view.id"
-        :view="view"
-      />
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Time Zone</th>
+          <th>Path</th>
+          <th>Referrer</th>
+          <th>Browser</th>
+          <th>Engine</th>
+          <th>OS</th>
+          <th>Vendor</th>
+          <th>Window</th>
+        </tr>
+      </thead>
+      <tbody>
+        <view-table-row
+          v-for="view in allViews"
+          :key="view.id"
+          :view="view"
+        />
+      </tbody>
     </table>
   </div>
 </template>
