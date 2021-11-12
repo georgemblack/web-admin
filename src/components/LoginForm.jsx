c6643ffcf7ebea0d675b1ee2a97eb5618de05372
George Black <contact@george.black>
2021-11-12 15:30:01 -0600

    Login form styles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -20,7 +20,8 @@ function LoginForm(props) {
   };
 
   return (
-    <div>
+    <div className="max-w-xs mx-auto">
+      <h2 className="mt-4 text-2xl text-center">Login</h2>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -28,6 +29,7 @@ function LoginForm(props) {
           placeholder="username"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
+          className="w-full px-4 py-2 mt-2 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded-full appearance-none focus:outline-none focus:bg-white focus:border-blue-500"
         />
         <input
           type="password"
@@ -35,8 +37,14 @@ function LoginForm(props) {
           placeholder="password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          className="w-full px-4 py-2 mt-2 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded-full appearance-none focus:outline-none focus:bg-white focus:border-blue-500"
         />
-        <button type="submit">Login</button>
+        <button
+          type="submit"
+          className="block px-4 py-2 mx-auto mt-2 bg-blue-500 rounded-full hover:bg-blue-700"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
