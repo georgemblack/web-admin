e5e0a06ec763e527d43615cdf9ae5ca390f9f0b1
George Black <contact@george.black>
2021-11-13 17:12:29 -0600

    More minor style updates

--- src/components/LikeTable.jsx
+++ src/components/LikeTable.jsx
@@ -22,7 +22,7 @@ function LikeTable(props) {
       {likes.map((like) => (
         <div className="flex flex-col justify-between px-3 py-2 mt-2 bg-gray-800 rounded lg:flex-row">
           <p className="text-white">{like.title}</p>
-          <div className="flex items-end justify-between">
+          <div className="flex items-end justify-between mt-1 lg:m-0">
             <div className="inline-block text-white opacity-25 lg:mr-4 lg:opacity-100">
               <Time timestamp={like.timestamp._seconds} />
             </div>
