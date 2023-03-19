8dbbea9e4f2d94ed6f1a9b57e59bf3189898801c
George Black <contact@george.black>
2023-03-19 17:02:45 +0000

    Update mock API

--- mock/src/index.js
+++ mock/src/index.js
@@ -6,8 +6,23 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 9000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.post("/auth", (req, res) => {
-  res.status(200).send({ token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NzYxNjQwMjEsImV4cCI6MTcwNzcwMDAyMSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.7D8EeZkX8uNKMiMuUXpd0isKQwvFK3c3BTiMMnQvzZY"});
+  res.status(200).send({
+    token:
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NzYxNjQwMjEsImV4cCI6MTcwNzcwMDAyMSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.7D8EeZkX8uNKMiMuUXpd0isKQwvFK3c3BTiMMnQvzZY",
+  });
+});
+
+app.post("/builds", (req, res) => {
+  res.status(200).send({
+    buildID: "bogus",
+  });
 });
 
 app.get("/likes", async (req, res) => {
@@ -18,11 +33,6 @@ app.get("/likes", async (req, res) => {
 app.get("/posts", async (req, res) => {
   res.header("Content-Type", "application/json");
   res.status(200).send({ posts: posts });
-});
-
-// Requests to R2 cloud storage
-app.put("/", async (req, res) => {
-  res.status(200).send();
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
