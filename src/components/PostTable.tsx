e0cdf8b12ca921458f49bd54b79d15bd07ffcc84
George Black <contact@george.black>
2023-03-24 15:38:56 -0500

    Type fix

--- src/components/PostTable.tsx
+++ src/components/PostTable.tsx
@@ -36,7 +36,7 @@ function PostTable() {
             className="mt-2 flex flex-col justify-between rounded bg-gray-800 px-3 py-2 lg:flex-row"
           >
             <p className="text-white">
-              {post.metadata.title} {post.metadata.draft ? draftTag : ""}
+              {post.title} {post.draft ? draftTag : ""}
             </p>
             <div className="mt-1 flex items-end justify-between lg:m-0">
               <div className="inline-block text-white opacity-25 lg:mr-4 lg:opacity-100">
